d9d0b286a543e319b9966038bf6694698b82b854
puldyastin <puldyastin@gmail.com>
2021-10-03 11:55:50 +0800

    export updated db

--- db/csc_db.sql
+++ db/csc_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 10, 2021 at 09:22 AM
+-- Generation Time: Oct 03, 2021 at 05:55 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -24,10 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `events`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `eName` varchar(255) NOT NULL,
+  `eDescription` varchar(255) NOT NULL,
+  `eLocation` varchar(255) NOT NULL,
+  `startdate` varchar(255) NOT NULL,
+  `startime` varchar(255) NOT NULL,
+  `enddate` varchar(255) NOT NULL,
+  `endtime` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `eName`, `eDescription`, `eLocation`, `startdate`, `startime`, `enddate`, `endtime`) VALUES
+(1, 'asdas', 'dadas', 'dasdas', 'dasd', 'asdad', 'asdasd', 'asdas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `logout` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentsAcc`
+--
+
+CREATE TABLE `studentsAcc` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `course` varchar(255) NOT NULL,
@@ -35,21 +73,65 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `studentsAcc`
 --
 
-INSERT INTO `user` (`id`, `name`, `course`, `studentId`) VALUES
+INSERT INTO `studentsAcc` (`id`, `name`, `course`, `studentId`) VALUES
 (3, 'paul', 'BSIS-4A', '2000'),
 (4, 'joshua', 'BSIS-4A', '2000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `picture` longblob NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `picture`, `role`, `status`) VALUES
+(1, 'Joshua Buban', 'jusswaaa', '1234', 0x313633323937313735312d, 'President', 'Online'),
+(2, 'Ma. Roseanne Pandaan', 'roseanne', '1234', 0x313633323937313835312d, 'Vice President', 'Offline'),
+(3, 'Eddessa Joy Legson', 'eddessa', '1234', 0x313633323937313934312d, 'Secretary', 'Offline'),
+(5, 'Tricia Kaye Moya', 'tricia', '1234', 0x313633323937323031352d, 'Representative', 'Offline');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Indexes for table `events`
 --
-ALTER TABLE `user`
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `studentsAcc`
+--
+ALTER TABLE `studentsAcc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -57,10 +139,28 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `events`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `studentsAcc`
+--
+ALTER TABLE `studentsAcc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

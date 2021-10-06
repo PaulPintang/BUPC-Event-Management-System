99a1fd6b6bb6007a9ad6d68c27ec7930aec407ac
puldyastin <puldyastin@gmail.com>
2021-10-06 12:05:42 +0800

    UI: homepage responsive

--- admin/home.php
+++ admin/home.php
@@ -40,7 +40,7 @@
     
 </head>
 <body>
-    <section class="">
+    <section class="px-6">
         <div class="flex mx-auto container justify-between items-center">
             <div class="flex items-center gap-3 py-7">
                  <img class="h-8 w-auto sm:h-10" src="./images/index.jpg" >
@@ -50,13 +50,13 @@
             </div>
             <div class="flex items-center gap-2">
                 <?php while ($row = mysqli_fetch_array($currentUser)) { ?>
-                    <div class="flex items-center text-sm text-gray-600">
+                    <div class="hidden md:flex items-center text-sm text-gray-600">
                        <p> Hello,</p>
                        <img src="./images/wave.gif" alt="" style="width: 30px">
                        <span> <?php echo $row['name']?> </span>
                     </div>
                     <div class="flex-shrink-0 h-10 w-10 pl-2">
-                        <img class="h-10 w-10 rounded-full" src="./images/undraw_profile_3.svg" alt="">
+                        <img class="h-10 w-10 rounded-full block " src="./images/undraw_profile_3.svg" alt="">
                     </div>
                 <?php } ?>
                 <div class="opacity-10">|</div>
@@ -97,21 +97,21 @@
         </div>
     </section>
 
-    <section class="bg-gray-50 pb-10">
-        <div class="flex gap-7 py-10 container mx-auto">
-            <div class="bg-white rounded-lg h-96 w-full flex justify-center items-center shadow-sm">
-                <div class="h-auto flex justify-between p-10">
-                    <div class="w-2/4">
-                        <h1 class="text-4xl font-bold text-gray-700"> <span class="text-yellow-400">BUPC</span>  College Student Council <span class="border-b-2 border-yellow-100">Admin page</span> </h1>
-                        <p class="text-gray-400 py-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, dicta. Lorem ipsum dolor sit amet.</p>
+    <section class="bg-gray-50 mx-6 lg:mx-0">
+        <div class="block md:flex lg:flex gap-7 gap-y-7 py-10 container mx-auto">
+            <div class="bg-white rounded-lg h-96 w-full flex justify-center items-center shadow-sm mb-9 md:mb-0 lg:mb-0">
+                <div class="h-auto block md:flex lg:flex justify-center lg:justify-between md:justify-between p-10 text-center md:text-right lg:text-left w-full">
+                    <div class="w-full lg:w-2/4">
+                        <h1 class=" text-3xl lg:text-4xl font-bold text-gray-700"> <span class="text-yellow-400">BUPC</span>  College Student Council <span class="border-b-2 border-yellow-100">Admin page</span> </h1>
+                        <p class="text-gray-400 py-5 text-sm lg:text-base">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, dicta. Lorem ipsum dolor sit amet.</p>
                         <a href="../index.php" class="py-2 px-5 bg-yellow-400 rounded text-white hover:bg-yellow-300 transition-all">Official website</a>
                     </div>
-                    <div class="w-2/4">
+                    <div class="w-2/4 hidden lg:block">
                           <img src="./images/admin.svg" alt="" style="width: 100%">
                     </div>
                 </div>
             </div>
-            <div class="bg-white rounded-lg p-6 shadow-sm" style="width: 500px">
+            <div class="bg-white rounded-lg p-6 shadow-sm">
                 <div class="flex justify-between pb-5">
                     <h1 class="font-medium text-gray-700">Users</h1>
                     <div>
@@ -123,10 +123,10 @@
                  <table class="min-w-full divide-y divide-gray-200">
                     <thead class="">
                         <tr>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th scope="col" class="md:px-6 lg:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Name
                         </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden md:block lg:block">
                             Role
                         </th>
                         <th scope="col" class="relative px-3 py-3">
@@ -137,7 +137,7 @@
                    <tbody class="bg-white divide-y divide-gray-200">
                     <?php while ($row = mysqli_fetch_array($users)) { ?>
                         <tr>
-                            <td class="px-6 py-2 whitespace-nowrap">
+                            <td class="md:px-6 lg:px-6 py-2 whitespace-nowrap">
                                 <div class="flex items-center">
                                 <div class="flex-shrink-0 h-10 w-10">
                                     <img class="h-10 w-10 rounded-full" src="./images/undraw_profile_3.svg" alt="">
@@ -147,13 +147,13 @@
                                 </div>
                                 </div>
                             </td>
-                            <td class="px-4 py-4 whitespace-nowrap">
+                            <td class="px-4 py-4 whitespace-nowrap  hidden md:block lg:block">
                                 <span class="px-2 inline-flex leading-5 font-semibold rounded-full bg-green-100 text-green-800" style="font-size: 10px">
                                 <?php echo $row['role']?>
                                 </span>
                             </td>
 
-                            <td class="px-6 py-4 whitespace-nowrap text-right font-medium" style="font-size: 13px">
+                            <td class="md:px-6 lg:px-6 py-4 whitespace-nowrap text-right font-medium" style="font-size: 13px">
                                 <a href="#edit<?php echo $row['id'];?>" data-toggle="modal" class="text-indigo-600 hover:text-indigo-900 w-full transition-all">Edit</a>
                             </td>
                         </tr>
@@ -163,7 +163,7 @@
             </div>
         </div>
 
-        <div class="container mx-auto">
+        <!-- <div class="container mx-auto">
             <div class="bg-white rounded-lg h-screen p-8">
                 <div class="flex justify-between">
                     <h1>Calendar</h1>
@@ -180,7 +180,7 @@
                 </div>
                 <div id='calendar'></div>
             </div>
-        </div>
+        </div> -->
      <div>
      </div>
     </section>

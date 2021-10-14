620d16908ef4077abce1878ec4d2ae92bfbfadb8
puldyastin <puldyastin@gmail.com>
2021-10-14 16:59:16 +0800

    need to branch out

--- admin/events/events.php
+++ admin/events/events.php
@@ -10,10 +10,9 @@ scrollbar-width: none;  /* Firefox */
 }
 
 </style>
-<div class="block md:flex lg:flex gap-5 py-10 mx-6 md:mx-0 lg:mx-0 container w-full">
-    
-    <div class="bg-white md:rounded-lg lg:rounded-lg h-full w-full p-6 shadow-sm">
-    <!-- start -->
+<div class`="block md:flex lg:flex gap-5 py-10 mx-6 md:mx-0 lg:mx-0 container w-full">
+    <div class="bg-white md:rounded-lg lg:rounded-lg h-96 w-full p-6 shadow-sm">
+    <!-- start -->  
     <div class="flex justify-between">
         <div class="flex items-center gap-4 pb-3">
                 <div class="flex gap-3 items-center">
@@ -65,7 +64,7 @@ scrollbar-width: none;  /* Firefox */
                     <th scope="col" class="hidden md:table-cell lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
                         <span class="font-extrabold text-red-400">Edit</span> date/time
                     </th>
-                     <th scope="col" class=" hidden md:table-cell lg:table-cell px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
+                     <th scope="col" class=" hidden md:table-cell lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
                         Rules
                     </th>
                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
@@ -119,7 +118,7 @@ scrollbar-width: none;  /* Firefox */
                             <small>- <?php echo $endtime?></small>
                         </div>
                     </td>
-                    <td class="hidden md:table-cell lg:table-cell px-1 py-2 whitespace-nowrap">
+                    <td class="hidden md:table-cell lg:table-cell px-6 py-2 whitespace-nowrap">
                         <?php
 
                             if ($row['rules'] == 'Required') {
@@ -136,38 +135,13 @@ scrollbar-width: none;  /* Firefox */
                         
                         ?>
                     </td>
-                    <td class="text-right md:px-3 lg:px-3 py-2 whitespace-nowrap space-x-2">
+                    <td class="text-right md:px-6 lg:px-6 py-2 whitespace-nowrap space-x-2">
                         <!-- <a href="#view<?php echo $row['id'];?>" data-toggle="modal" class="sm:hidden md:hidden lg:hidden"><i class="fas fa-info text-gray-400 cursor-pointer hover:text-blue-300 transition-all" style="font-size:12px"></i></a> -->
                         <a href="#viewE<?php echo $row['id'];?>" data-toggle="modal" class=" sm:hidden md:hidden lg:hidden text-blue-400 hover:text-white hover:bg-blue-400 w-full px-3 py-1 bg-blue-50 font-semibold rounded transition-colors text-xs">View</a>
                         <a href="#viewE<?php echo $row['id'];?>" data-toggle="modal" class="hidden sm:inline md:inline lg:inline text-blue-400 hover:text-white hover:bg-blue-400 w-full px-3 py-1 bg-blue-50 font-semibold rounded transition-colors text-xs">View More</a>
                         <a href="#edit<?php echo $row['id'];?>" data-toggle="modal" >
-                            <i class="far fa-edit text-gray-400 cursor-pointer hover:text-blue-300 transition-all pr-2 md:pr-0 lg:pr0" style="font-size: 13px"></i>
+                            <i class="far fa-edit text-gray-400 cursor-pointer hover:text-blue-300 transition-all" style="font-size: 13px"></i>
                         </a> 
-                    </td>
-                    <td>
-                        <?php
-                            date_default_timezone_set("Asia/Manila");
-                            $currentDate = date("Y-m-d");
-                            $tommorow = date('Y-m-d', strtotime(' +1 day'));
-                            $upcoming = date('Y-m-d', strtotime(' +2 day'));
-                            if ($row['startdate'] == $currentDate) {
-                                echo '
-                                    <div class="bg-green-300 w-8 h-2 mt-1"></div>
-                                ';
-                            }else if($row['startdate'] == $tommorow){
-                                echo'
-                                    <div class="bg-blue-300 w-8 h-2 mt-1"></div>
-                                ';
-                            }else if($row['startdate'] >= $upcoming){
-                                echo'
-                                    <div class="bg-pink-300 w-8 h-2 mt-1"></div>
-                                ';
-                            }else {
-                                echo '
-                                    <div class="bg-yellow-300 w-8 h-2 mt-1"></div>
-                                ';
-                            }
-                        ?>
                     </td>
                 </tr>
             <?php }?>
@@ -199,5 +173,4 @@ scrollbar-width: none;  /* Firefox */
   <!-- <script src="../calendar-10/js/popper.js"></script> -->
   <!-- <script src="../calendar-10/js/bootstrap.min.js"></script> -->
   <script src="../calendar/js/main.js"></script>
-
 </div>

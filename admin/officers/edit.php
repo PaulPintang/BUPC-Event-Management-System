49d499029b02f4c4c70788f4afb4d0d5a4cec7c1
puldyastin <puldyastin@gmail.com>
2021-10-20 18:55:03 +0800

    changes

--- admin/officers/edit.php
+++ admin/officers/edit.php
@@ -229,9 +229,9 @@ scrollbar-width: none;  /* Firefox */
                             </div>
                             <div class="flex justify-center">
                                 <div style="font-size: 14px">
-                                    <button type="button" data-dismiss="modal" class="px-6 py-2 bg-gray-100 rounded text-gray-500">
+                                    <a href="../officers" class="px-6 py-2 bg-gray-100 rounded text-gray-500">
                                         Cancel
-                                    </button>
+                                    </a>
                                     <button class="px-6 bg-yellow-500 hover:bg-yellow-400 py-2 text-white rounded ml-3" name="save" type="submit">
                                         Submit
                                     </button>
